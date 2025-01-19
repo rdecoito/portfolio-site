5a40a3f9e383d2d3166920cd6b5e0a9f6fb10caf
Landon DeCoito <landondecoito2021@gmail.com>
2025-01-18 22:15:11 -0500

    rewrote using 11ty

--- src/components/Card.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Card = styled.div(({ theme }) => ({
-    backgroundColor: theme.color('--card-background'),
-    boxShadow: `0 0 ${theme.gap(1)} ${theme.color('--shadow-color')}`,
-    borderRadius: theme.gap(1),
-    padding: theme.padding(3)
-}));
